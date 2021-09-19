6cce05d88d8f1b8c2037b9842a03ac853422c7f3
Xiwen Cheng <x@cinaq.com>
2021-09-19 10:17:14 +0200

    Refactored to use flags and logging

--- README.md
+++ README.md
@@ -20,3 +20,26 @@ Bundle-Vendor: Apache Software Foundation
 Bundle-SymbolicName: org.apache.velocity
 Bundle-Version: 1.7
 ```
+
+jar format 3:
+```
+$ cat META-INF/MANIFEST.MF
+Manifest-Version: 1.0
+Export-Package: com.google.gson;version=2.2.4, com.google.gson.annotat
+ ions;version=2.2.4, com.google.gson.reflect;version=2.2.4, com.google
+ .gson.stream;version=2.2.4, com.google.gson.internal;version=2.2.4, c
+ om.google.gson.internal.bind;version=2.2.4
+Bundle-ClassPath: .
+Built-By: inder
+Bundle-Name: Gson
+Created-By: Apache Maven 3.0.4
+Bundle-RequiredExecutionEnvironment: J2SE-1.5
+Bundle-Vendor: Google Gson Project
+Bundle-ContactAddress: http://code.google.com/p/google-gson/
+Bundle-Version: 2.2.4
+Build-Jdk: 1.7.0_21
+Bundle-ManifestVersion: 2
+Bundle-Description: Google Gson library
+Bundle-SymbolicName: com.google.gson
+Archiver-Version: Plexus Archiver
+```
--- cmd/mendix-userlib-cleaner/main.go
+++ cmd/mendix-userlib-cleaner/main.go
@@ -3,21 +3,32 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"strings"
 
 	"archive/zip"
 
+	"flag"
 	"io"
 	"os"
 	"path/filepath"
 	"strconv"
+
+	"github.com/op/go-logging"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+var log = logging.MustGetLogger("main")
+
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
 )
 
 type JarProperties struct {
 	version       string
 	versionNumber int
 	filePath      string
+	fileName      string
 	packageName   string
 	name          string
 	vendor        string
@@ -26,77 +37,63 @@ type JarProperties struct {
 
 func main() {
 
-	targetDir := "/Users/xcheng/Downloads/userlib"
+	flag.String("target", ".", "Path to userlib. Default: . (current directory)")
+	flag.Bool("clean", false, "Turn on to actually remove the duplicate jars. Default: false")
+	flag.Bool("verbose", false, "Turn on to see debug information. Default: false")
+
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+
+	targetDir := viper.GetString("target")
+	clean := viper.GetBool("clean")
+	verbose := viper.GetBool("verbose")
+
+	backend := logging.NewLogBackend(os.Stderr, "", 0)
+	backendFormatter := logging.NewBackendFormatter(backend, format)
+
+	// Set the backends to be used.
+	logging.SetBackend(backendFormatter)
+	if verbose {
+		logging.SetLevel(logging.DEBUG, "main")
+	} else {
+		logging.SetLevel(logging.INFO, "main")
+	}
+
+	jars := listAllJars(targetDir)
+	keepJars := computeJarsToKeep(jars)
+	count := cleanJars(clean, jars, keepJars)
+
+	if clean {
+		log.Infof("Total files removed: %d", count)
+	} else {
+		log.Infof("Would have removed: %d files", count)
+		log.Infof("Use --clean to actually remove above file(s)")
+	}
+
+}
+
+func listAllJars(targetDir string) []JarProperties {
+	log.Info("Finding and parsing JARs")
 	files, err := ioutil.ReadDir(targetDir)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	jars := []JarProperties{}
-
 	for _, f := range files {
 		if strings.HasSuffix(f.Name(), ".jar") {
-			//log.Println("Processing " + f.Name())
+			log.Debugf("Processing JAR: %v", f.Name())
 			filePath := filepath.Join(targetDir, f.Name())
-			jarProp := getJarProperties(filePath)
+			jarProp := getJarProps(filePath)
 			if strings.Compare(jarProp.filePath, "") != 0 {
 				jars = append(jars, jarProp)
-				//log.Println(jarProp)
 			}
 		}
 	}
-
-	// remove duplicates
-	var keepJars = make(map[string]JarProperties)
-
-	for _, jar1 := range jars {
-		//log.Println("Checking " + jar1.filePath)
-		if _, ok := keepJars[jar1.packageName]; !ok {
-			keepJars[jar1.packageName] = jar1
-		}
-		packageName := jar1.packageName
-
-		// find latest
-		for _, jar2 := range jars {
-			latestJar := keepJars[packageName]
-			if strings.Compare(jar1.filePath, jar2.filePath) == 0 {
-				// skip self
-				continue
-			}
-			if strings.Compare(latestJar.filePath, jar2.filePath) == 0 {
-				// skip self
-				continue
-			}
-			if strings.Compare(packageName, jar2.packageName) == 0 {
-				goodFileSuffix := fmt.Sprintf("%s%s", jar2.version, ".jar")
-				if latestJar.versionNumber == jar2.versionNumber && strings.HasSuffix(jar2.filePath, goodFileSuffix) {
-					// same version and has better file name
-					log.Println("Better file name: " + jar2.filePath)
-					keepJars[packageName] = jar2
-				} else if latestJar.versionNumber < jar2.versionNumber {
-					// newer version found
-					log.Println("Newer version: " + jar2.filePath)
-					keepJars[packageName] = jar2
-				}
-			}
-		}
-	}
-
-	count := 0
-	for _, jar := range jars {
-		jarToKeep := keepJars[jar.packageName]
-		if strings.Compare(jar.filePath, jarToKeep.filePath) != 0 {
-			if _, err := os.Stat(jar.filePath); err == nil {
-				log.Println("Removing duplicate of " + jar.packageName + ": " + jar.filePath)
-				os.Remove(jar.filePath)
-				count++
-			}
-		}
-	}
-	log.Println("Total files removed: " + fmt.Sprint(count))
+	return jars
 }
 
-func getJarProperties(filePath string) JarProperties {
+func getJarProps(filePath string) JarProperties {
 
 	archive, err := zip.OpenReader(filePath)
 	if err != nil {
@@ -131,7 +128,7 @@ func getJarProperties(filePath string) JarProperties {
 
 		b, err := ioutil.ReadFile(fileName)
 		if err != nil {
-			log.Print(err)
+			log.Warningf("Unable to read file: %v", err)
 		}
 
 		err = os.Remove(fileName)
@@ -143,21 +140,23 @@ func getJarProperties(filePath string) JarProperties {
 		text := string(b)
 		jar1 := parseManifest(filePath, text)
 		if jar1.packageName != "" {
+			log.Debugf("Parsed properties from MANIFEST: %v", jar1)
 			return jar1
 		}
 		jar2 := parsePOM(filePath, text)
 		if jar2.packageName != "" {
+			log.Debugf("Parsed properties from POM: %v", jar2)
 			return jar2
 		}
 	}
-	log.Println("Failed to parse " + filePath)
+	log.Warningf("Failed to parse %v", filePath)
 
 	return JarProperties{filePath: ""}
 }
 
 func parseManifest(filePath string, text string) JarProperties {
 	lines := strings.Split(text, "\n")
-	jarProp := JarProperties{filePath: filePath, packageName: ""}
+	jarProp := JarProperties{filePath: filePath, packageName: "", fileName: filepath.Base(filePath)}
 	for _, line := range lines {
 		line = strings.TrimSpace(line)
 		pair := strings.Split(line, ": ")
@@ -180,7 +179,7 @@ func parseManifest(filePath string, text string) JarProperties {
 
 func parsePOM(filePath string, text string) JarProperties {
 	lines := strings.Split(text, "\n")
-	jarProp := JarProperties{filePath: filePath, packageName: ""}
+	jarProp := JarProperties{filePath: filePath, packageName: "", fileName: filepath.Base(filePath)}
 	groupId := ""
 	artifactId := ""
 	for _, line := range lines {
@@ -200,4 +199,63 @@ func parsePOM(filePath string, text string) JarProperties {
 		jarProp.packageName = groupId + "." + artifactId
 	}
 	return jarProp
+}
+
+func computeJarsToKeep(jars []JarProperties) map[string]JarProperties {
+	log.Info("Computing duplicates")
+	var keepJars = make(map[string]JarProperties)
+
+	for _, jar1 := range jars {
+		//log.Println("Checking " + jar1.filePath)
+		if _, ok := keepJars[jar1.packageName]; !ok {
+			keepJars[jar1.packageName] = jar1
+		}
+		packageName := jar1.packageName
+
+		// find latest
+		for _, jar2 := range jars {
+			latestJar := keepJars[packageName]
+			if strings.Compare(jar1.filePath, jar2.filePath) == 0 {
+				// skip self
+				continue
+			}
+			if strings.Compare(latestJar.filePath, jar2.filePath) == 0 {
+				// skip self
+				continue
+			}
+			if strings.Compare(packageName, jar2.packageName) == 0 {
+				goodFileSuffix := fmt.Sprintf("%s%s", jar2.version, ".jar")
+				if latestJar.versionNumber == jar2.versionNumber && strings.HasSuffix(jar2.filePath, goodFileSuffix) {
+					log.Infof("Preferring file %v over %v", jar2.fileName, latestJar.fileName)
+					keepJars[packageName] = jar2
+				} else if latestJar.versionNumber < jar2.versionNumber {
+					log.Infof("Found newer %v over %v", jar2.fileName, latestJar.fileName)
+					keepJars[packageName] = jar2
+				}
+			}
+		}
+	}
+	return keepJars
+}
+
+func cleanJars(remove bool, jars []JarProperties, keepJars map[string]JarProperties) int {
+	log.Info("Cleaning...")
+	count := 0
+	for _, jar := range jars {
+		jarToKeep := keepJars[jar.packageName]
+		if strings.Compare(jar.filePath, jarToKeep.filePath) != 0 {
+			if _, err := os.Stat(jar.filePath); err == nil {
+				if remove {
+					log.Warningf("Removing duplicate of %v: %v", jar.packageName, jar.fileName)
+					os.Remove(jar.filePath)
+				} else {
+					log.Warningf("Would remove duplicate of %v: %v", jar.packageName, jar.fileName)
+				}
+				count++
+			}
+		} else {
+			log.Debugf("Keeping jar: %v", jar)
+		}
+	}
+	return count
 }
